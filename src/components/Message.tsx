a584f4f8eac0d37b74b6570573c26637db2a5f22
andrewevans0102 <andrewevans0102@gmail.com>
2025-03-05 14:22:16 -0500

    fixing message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,7 +6,7 @@ const Message = () => {
             <h1>Hello!</h1>
             <div className="message__body">
                 <p>
-                    {`Rhythm and Binary has been retired. All posts are can now be seen at `}
+                    {`Rhythm and Binary has been retired. All posts have been moved to `}
                     <a href="https://www.andrewevans.dev">AndrewEvans.dev</a>
                 </p>
             </div>
